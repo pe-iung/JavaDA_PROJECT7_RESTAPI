769d0b712f0776c29d5bd715ff38f33b33f4f4b5
pierre-edouard iung <contact@pierre-edouard.tech>
2025-04-03 18:58:46 +0200

    feat: add feature ruleName update

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,47 +1,122 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.controllers.DTO.RuleNameRequest;
 import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.services.CrudService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+
+    private final CrudService<RuleName> ruleNameService;
 
     @RequestMapping("/ruleName/list")
     public String home(Model model)
     {
-        // TODO: find all RuleName, add to model
+        List<RuleName> ruleNames = ruleNameService.findAll();
+        model.addAttribute("ruleNames", ruleNames);
         return "ruleName/list";
     }
 
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(RuleNameRequest ruleNameRequest) {
         return "ruleName/add";
     }
 
     @PostMapping("/ruleName/validate")
-    public String validate(@Validated RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
+    public String validate(
+            @Validated RuleNameRequest ruleNameRequest,
+            BindingResult result,
+            Model model,
+            RedirectAttributes redirectAttributes) {
+
+        if (result.hasErrors()) {
+            return "rulename/add";
+        }
+        try {
+
+
+        RuleName newRuleName = new RuleName(
+                ruleNameRequest.getName(),
+                ruleNameRequest.getDescription(),
+                ruleNameRequest.getJson(),
+                ruleNameRequest.getTemplate(),
+                ruleNameRequest.getSqlStr(),
+                ruleNameRequest.getSqlPart()
+        );
+        ruleNameService.save(newRuleName);
+        redirectAttributes.addFlashAttribute(
+                "successMessage",
+                "new ruleName added successfully");
+        }
+        catch (Exception e) {
+            redirectAttributes.addFlashAttribute(
+                    "errorMessage",
+                    "ERROR : new ruleName not added because of error : " + e);
+        }
+
+        return "redirect:/ruleName/list";
+
     }
 
     @GetMapping("/ruleName/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+
+        RuleName ruleName = ruleNameService.getById(id);
+        RuleNameRequest ruleNameRequest = new RuleNameRequest(
+                ruleName.getName(),
+                ruleName.getDescription(),
+                ruleName.getJson(),
+                ruleName.getTemplate(),
+                ruleName.getSqlStr(),
+                ruleName.getSqlPart()
+        );
+        model.addAttribute("ruleNameRequest", ruleNameRequest);
+        model.addAttribute("ruleNameId",id);
         return "ruleName/update";
     }
 
     @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Validated RuleName ruleName,
-                             BindingResult result, Model model) {
+    public String updateRuleName(
+            @PathVariable("id") Integer id,
+            @Validated RuleNameRequest ruleNameRequest,
+            BindingResult result,
+            Model model,
+            RedirectAttributes redirectAttributes) {
         // TODO: check required fields, if valid call service to update RuleName and return RuleName list
+        if (result.hasErrors()) {
+            return "/ruleName/list";
+        }
+
+        model.addAttribute("ruleNameId",id);
+        model.addAttribute("ruleNameRequest", ruleNameRequest);
+        try {
+            RuleName updatedRuleName = ruleNameService.getById(id);
+            updatedRuleName.setName(ruleNameRequest.getName());
+            updatedRuleName.setDescription(ruleNameRequest.getDescription());
+            updatedRuleName.setJson(ruleNameRequest.getJson());
+            updatedRuleName.setTemplate(ruleNameRequest.getTemplate());
+            updatedRuleName.setSqlStr(ruleNameRequest.getSqlStr());
+            updatedRuleName.setSqlPart(ruleNameRequest.getSqlPart());
+            ruleNameService.update(updatedRuleName);
+
+            redirectAttributes.addFlashAttribute("successMessage",
+                    "ruleName with id = " + id + " succesfully updated");
+        }
+        catch (Exception e) {
+            redirectAttributes.addFlashAttribute("errorMessage",
+                    "ERROR: ruleName with id = " + id + " NOT updated because of error : " + e);
+
+        }
         return "redirect:/ruleName/list";
     }
 
